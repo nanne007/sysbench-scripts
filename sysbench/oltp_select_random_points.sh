ce3e58d94a75113c3c8800b8a9c440c679104414
caojiafeng <funfriendcjf@gmail.com>
2017-12-25 19:39:21 +0800

    init

---
+++ sysbench/oltp_select_random_points.sh
@@ -0,0 +1,19 @@
+. ./conf.sh
+
+
+if [[ $db_driver = "mysql" ]];
+then
+if [[ ${mysql_password} = "" ]];
+then
+    mysql -h ${mysql_host} -P ${mysql_port} -u${mysql_user} -e "CREATE DATABASE IF NOT EXISTS ${mysql_db}"
+else
+    mysql -h ${mysql_host} -P ${mysql_port} -u${mysql_user} -p{$mysql_password} -e "CREATE DATABASE IF NOT EXISTS ${mysql_db}"
+fi
+fi
+sysbench --db-driver=$db_driver \
+         --mysql-host=$mysql_host --mysql-port=$mysql_port \
+         --mysql-user=$mysql_user --mysql-password=$mysql_password --mysql-db=$mysql_db \
+         --threads=$threads  --report-interval=$report_interval --histogram=$histogram \
+         --table_size=$table_size --tables=1 \
+         --random_points=100 \
+         /usr/share/sysbench/select_random_points.lua run
